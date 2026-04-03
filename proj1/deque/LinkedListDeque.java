c679777aec6c223e283d3e5d83d5971cdf4a401c
Tianxiang <780215148@qq.com>
2026-04-03 18:54:38 +0800

    add:some method

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,53 @@
+package deque;
+
+public class LinkedListDeque<T> {
+    private int size;
+    private IntNode sentinel;
+
+    public class IntNode {
+        public IntNode prev;
+        public T item;
+        public IntNode next;
+    }
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel =
+    }
+
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+
+    }
+
+    public T removeFirst() {
+
+    }
+
+    public T removeLast() {
+
+    }
+
+    public T get(int index) {
+
+    }
+
+    public T getRecursive(int index) {
+
+    }
+}
